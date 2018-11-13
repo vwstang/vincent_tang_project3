aab3e3e0cea6c816fe1894125c7235302f301dd2
vwstang <vwstang6@gmail.com>
2018-11-13 16:15:40 -0500

    Updated destination and activity objects

--- js/script.js
+++ js/script.js
@@ -2,16 +2,19 @@
 // 3D array of vacation destinations
 const objDestinations = {
   urban: {
-    hot: [],
-    cold: []
+    hot: ["Hong Kong"],
+    varying: ["London"],
+    cold: ["Harbin"]
   },
   suburban: {
     hot: [],
+    varying: [],
     cold: ["Innsbruck", "Reykjavik"]
   },
   rural: {
     hot: [],
-    cold: []
+    varying: ["Óbidos"],
+    cold: ["Yellowknife"]
   }
 };
 
@@ -19,63 +22,95 @@ const objDestinations = {
 const objActivities = {
   Oslo: {
     country: "Norway",
+    population: 673469,
+    censusyear: 2018,
     sightseeing: [],
     food: []
   },
   London: {
     country: "Great Britain",
+    population: 0,
+    censusyear: 0,
     sightseeing: [],
     food: []
   },
   Paris: {
     country: "France",
+    population: 0,
+    censusyear: 0,
     sightseeing: [],
     food: []
   },
   Bangalore: {
     country: "India",
+    population: 12339447,
+    censusyear: 2017,
     sightseeing: [],
     food: []
   },
   "Hong Kong": {
     country: "China",
+    population: 7448900,
+    censusyear: 2018,
     sightseeing: [],
     food: []
   },
   "San Francisco": {
     country: "USA",
+    population: 0,
+    censusyear: 0,
     sightseeing: [],
     food: []
   },
   Óbidos: {
     country: "Portugal",
+    population: 11617,
+    censusyear: 2015,
     sightseeing: [],
     food: []
   },
   Reykjavík: {
     country: "Iceland",
+    population: 122853,
+    censusyear: 2016,
     sightseeing: [],
     food: []
   },
   Harbin: {
     country: "China",
+    population: 10635971,
+    censusyear: 2010,
     sightseeing: [],
     food: []
   },
   Innsbruck: {
     country: "Austria",
+    population: 0,
+    censusyear: 0,
     sightseeing: [],
     food: []
   },
   Yellowknife: {
     country: "Canada",
+    population: 0,
+    censusyear: 0,
     sightseeing: [],
     food: []
   },
   Staycation: {
     country: "the safety and comfort of your own home",
-    sightseeing: [],
-    food: []
+    population: "your family members",
+    censusyear: "today",
+    sightseeing: [
+      "watch TV",
+      "play video games",
+      "play with your dog",
+      "play with your cat"
+    ],
+    food: [
+      "Kraft Dinner",
+      "instant noodles"
+    ]
   }
 }
 
@@ -88,3 +123,7 @@ const randObjKey = obj => {
   const keys = Object.keys(obj);
   return keys[Math.floor(Math.random() * keys.length)];
 }
+
+//-- Random Array Element --//
+// Takes in an array argument and returns a random element within the array.
+const randArrElem = arr => arr[Math.floor(Math.random() * arr.length)];
